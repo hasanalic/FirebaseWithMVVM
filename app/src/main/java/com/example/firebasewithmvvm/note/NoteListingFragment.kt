faab31ed40d88c7eb91581ad405be92596aa34fb
Hasan Ali Caliskan <hasanali7295@gmail.com>
2023-03-10 19:11:17 +0300

    testing mvvm

--- app/src/main/java/com/example/firebasewithmvvm/note/NoteListingFragment.kt
+++ app/src/main/java/com/example/firebasewithmvvm/note/NoteListingFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import com.example.firebasewithmvvm.R
 import com.example.firebasewithmvvm.databinding.FragmentNoteListingBinding
+import com.example.firebasewithmvvm.model.Note
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -36,12 +37,18 @@ class NoteListingFragment : Fragment() {
         // viewmodel'daki getNotes() fonksiyonu, verileri yine viewmodel'daki _notes'a aktarır.
         // biz de "notes" liveData'sını observe ederek verinin durumunu takip ederiz.
         viewModel.notes.observe(viewLifecycleOwner){
-            // veri ile ilgili işlemler
+            setViews(it)
         }
     }
+
+    fun setViews(list: List<Note>) {
+        binding.id.text = list[0].id
+        binding.text.text = list[0].text
+        binding.date.text = list[0].date.toString()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
--- app/src/main/java/com/example/firebasewithmvvm/repository/NoteRepositoryImp.kt
+++ app/src/main/java/com/example/firebasewithmvvm/repository/NoteRepositoryImp.kt
@@ -2,13 +2,18 @@ package com.example.firebasewithmvvm.repository
 
 import com.example.firebasewithmvvm.model.Note
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class NoteRepositoryImp(
     val database: FirebaseFirestore
 ): NoteRepository {
 
     override fun getNotes(): List<Note> {
-        // firebase'den veri çekilir ve return edilir.
-        return arrayListOf()
+
+        return arrayListOf(Note(
+            "123456789",
+            "textextextextextextext",
+            Date()
+        ))
     }
 }
